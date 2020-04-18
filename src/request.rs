d9a1201264c42b42f4a5be058e15b6bfd31b2bf1
Tim Gabets <tim@gabets.ru>
2020-04-18 22:10:34 +0300

    no header

--- src/request.rs
+++ src/request.rs
@@ -12,21 +12,9 @@ pub struct Header {
     system_id: String,
 }
 
-impl Header {
-    pub fn new(message_id: i64, system_id: String) -> Header {
-        Header {
-            message_id: message_id,
-            system_id: system_id,
-        }
-    }
-}
-
 #[derive(Serialize, Debug)]
 #[serde(rename(serialize = "RequestInput"))]
 pub struct Request {
-    #[serde(rename(serialize = "FIXME"))]
-    #[serde(flatten)]
-    header: Header,
     #[serde(rename(serialize = "ISO8583-87"))]
     iso_fields: Value,
 }
@@ -34,14 +22,15 @@ pub struct Request {
 impl Request {
     pub fn new(iso_obj: Value) -> Request {
         Request {
-            header: Header::new(util::gen_message_id(), util::get_system_id()),
             iso_fields: iso_obj,
         }
     }
 
     pub fn serialize(&self) -> String {
         // TODO: return Result
-        to_string(&self).unwrap()
+        let serialized = to_string(&self).unwrap();
+        let serialized = format!("{:05}{}", serialized.len(), serialized);
+        serialized
     }
 }
 
@@ -50,16 +39,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn custom_header_serialization() {
-        let hdr = Header::new(1234, String::from("IDDQD"));
-        assert_eq!(
-            to_string(&hdr).unwrap(),
-            "<Header><MessageID>1234</MessageID><SystemID>IDDQD</SystemID></Header>"
-        );
-    }
-
-    #[test]
-    #[ignore]
     fn dummy_request_serialization() {
         let iso_data = r#"{
             "i000": "0100",
@@ -68,6 +47,6 @@ mod tests {
 
         let r: Request = Request::new(serde_json::from_str(&iso_data).unwrap());
 
-        assert_eq!(r.serialize(), "<RequestInput><Header><MessageID>430173293629234065</MessageID><SystemID>PROUST</SystemID></Header><ISO8583-87><i000>0100</i000><i002>521324******0895</i002></ISO8583-87></RequestInput>");
+        assert_eq!(r.serialize(), "00100<RequestInput><ISO8583-87><i000>0100</i000><i002>521324******0895</i002></ISO8583-87></RequestInput>");
     }
 }
