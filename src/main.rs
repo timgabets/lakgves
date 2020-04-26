9cf27c5bb170c2cc7251d5bc752bbf2f4717a4ff
Tim Gabets <tim@gabets.ru>
2020-04-26 23:04:12 +0300

    No gotham, hust hyper

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,40 @@
-extern crate gotham;
-extern crate mime;
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_xml_rs;
-
 //use serde_json::{Result, Value};
-use serde_json::Value;
 
 //use std::io::prelude::*;
 //use std::net::TcpStream;
 
-mod request;
-use request::Request;
+// mod request;
+// use request::Request;
 
-use gotham::helpers::http::response::create_response;
-use gotham::router::builder::*;
-use gotham::router::Router;
-use gotham::state::State;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::Server;
+use hyper::{Body, Client, Request, Response, Uri};
+use serde_json::Value;
+use std::net::SocketAddr;
 
-use hyper::{Body, Response, StatusCode};
-
+/*
 pub fn handler(state: State) -> (State, Response<Body>) {
     let iso_data = r#"
-		{
-			"i000": "0100",
-			"i002": "5536913******0961",
-			"i003": "300000",
-			"i004": "000000000000",
-			"i014": "2402",
-			"i018": "6011",
-			"i022": "0100",
-			"i023": "000",
-			"i025": "02",
-			"i026": "04",
-			"i032": "437783",
-			"i037": "293629234065",
-			"i041": "TERMID01",
-			"i042": "IDDQD MERCH ID",
-			"i043": "IDDQD AM. 341215574     341215574 MSKRU",
-			"i049": "643",
-			"i053": "9801100001000000",
-			"i120": "UD009TF0040431"
-		}"#;
+        {
+            "i000": "0100",
+            "i002": "5536913******0961",
+            "i003": "300000",
+            "i004": "000000000000",
+            "i014": "2402",
+            "i018": "6011",
+            "i022": "0100",
+            "i023": "000",
+            "i025": "02",
+            "i026": "04",
+            "i032": "437783",
+            "i037": "293629234065",
+            "i041": "TERMID01",
+            "i042": "IDDQD MERCH ID",
+            "i043": "IDDQD AM. 341215574     341215574 MSKRU",
+            "i049": "643",
+            "i053": "9801100001000000",
+            "i120": "UD009TF0040431"
+        }"#;
 
     let iso_obj: Value = serde_json::from_str(&iso_data).unwrap();
 
@@ -71,4 +64,43 @@ pub fn main() {
     let addr = "127.0.0.1:8080";
     println!("Listening to {}", addr);
     gotham::start(addr, router())
+}
+*/
+
+async fn serve_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    println!("Got request at {:?}", req.uri());
+    println!("Headers: {:?}", req.headers());
+
+    let body = hyper::body::to_bytes(req.into_body()).await?;
+    let iso_data = String::from_utf8(body.to_vec()).unwrap();
+
+    let iso_obj: Value = serde_json::from_str(&iso_data).unwrap();
+    println!("{:?}", iso_obj);
+
+    let url = "http://www.rust-lang.org/en-US/"
+        .parse::<Uri>()
+        .expect("Error parsing URL");
+    let res = Client::new().get(url).await?;
+    println!("request finished -- returning response");
+    Ok(res)
+}
+
+async fn run_server(addr: SocketAddr) {
+    println!("Listening on http://{}", addr);
+
+    let serve_future = Server::bind(&addr).serve(make_service_fn(|_| async {
+        {
+            Ok::<_, hyper::Error>(service_fn(serve_request))
+        }
+    }));
+
+    if let Err(e) = serve_future.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    run_server(addr).await;
 }
